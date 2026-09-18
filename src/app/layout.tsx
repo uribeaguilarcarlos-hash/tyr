7a8dc393a6af525f4fd3e6eea7c6d5be96afe75a
Carlo <carlo@tyrcargo.com>
2026-09-18 17:06:18 -0600

    chore: agregar google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,16 @@ export default function RootLayout({
           `}
         </Script>
         <Script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" strategy="afterInteractive" />
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5Z9BDTLD');
+          `}
+        </Script>
         {/* Google Analytics */}
         <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-Y1TZGMHJYW" />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -47,6 +57,16 @@ export default function RootLayout({
         </Script>
 </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5Z9BDTLD"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <div id="google_translate_element" style={{ display: 'none' }}></div>
         {children}
       </body>
